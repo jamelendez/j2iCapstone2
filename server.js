719abd471c44d6e566b2bfd869bdd76343ea9a7f
Jose Melendez <jose.melendez35@upr.edu>
2021-01-13 20:26:04 -0400

    bug fix

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const mongoose = require('mongoose')
 const path = require('path')
 const config = require('config');
-const path = require('path');
 
 
 const app = express();
